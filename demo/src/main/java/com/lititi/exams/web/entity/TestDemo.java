dfaeb26eae08f9c95bcf99be6a69a8d8d5b9e5d6
chenlong13507664259 <117248347+chenlong13507664259@users.noreply.github.com>
2025-10-30 14:02:03 +0800

    Update TestDemo.java
    Removed warning comments and author information from TestDemo.java

--- demo/src/main/java/com/lititi/exams/web/entity/TestDemo.java
+++ demo/src/main/java/com/lititi/exams/web/entity/TestDemo.java
@@ -3,17 +3,11 @@ package com.lititi.exams.web.entity;
 import java.io.Serializable;
 import lombok.Data;
 
-/**
- * 演示文件，请勿在此文件添加或者修改代码！！！
- * 演示文件，请勿在此文件添加或者修改代码！！！
- * 演示文件，请勿在此文件添加或者修改代码！！！
- */
+
 
 /**
  * (testDemo)实体类
  *
- * @author makejava
- * @since 2024-02-20 11:09:00
  */
 @Data
 public class TestDemo implements Serializable {

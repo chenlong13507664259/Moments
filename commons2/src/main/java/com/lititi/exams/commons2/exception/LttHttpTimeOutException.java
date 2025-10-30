7431056dbbc264f0400844a168ce581e282ac05b
chenlong13507664259 <117248347+chenlong13507664259@users.noreply.github.com>
2025-10-30 14:12:35 +0800

    Update LttHttpTimeOutException.java
    Removed unnecessary comments and author information.

--- commons2/src/main/java/com/lititi/exams/commons2/exception/LttHttpTimeOutException.java
+++ commons2/src/main/java/com/lititi/exams/commons2/exception/LttHttpTimeOutException.java
@@ -1,11 +1,6 @@
 package com.lititi.exams.commons2.exception;
 
-/**
- * 
- * @company 杭州利提提科技有限公司
- * @author Kevin
- * @version LttHttpTimeOutException.java, v 0.1 May 30, 2019 3:18:00 PM
- */
+
 public class LttHttpTimeOutException extends LttException {
 
     private static final long serialVersionUID = 1L;
